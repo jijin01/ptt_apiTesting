472c0f014ed389198021b7c64a5f59976a9b0c9d
Jijin <jijin.varghese@edstem.com>
2023-07-13 16:56:36 +0530

    Upload banking file

--- tests/upload_banking.py
+++ tests/upload_banking.py
@@ -1,11 +1,13 @@
 import login
 import requests
+import pandas as pd
 
 access_token = login.data["authenticationResult"]["AccessToken"]
 
 
 def test_banking_file_upload():
-    get_presigned_url = "https://dev-api.pttapp.com/api/banking/upload/presigned-url?clientId=635132a4dc29b4bad9e74d90&fileName=20230711-Test-api-Banking.xls"
+    get_presigned_url = "https://dev-api.pttapp.com/api/banking/upload/presigned-url?clientId" \
+                        "=63595f916a01b7ace483bcb1&fileName=20230711-Test-api-Banking."
     headers = {
         "Authorization": f"Bearer {access_token}"
     }
@@ -16,16 +18,31 @@ def test_banking_file_upload():
     data = response.json()
     presigned_url = data['presignedUrl']
     file_id = data['fileId']
-    with open('C:/Users/91812/pttApp-apiTesting/tests/test_file/20230711-Test-api-Banking.xls', 'rb') as file:
-        response = requests.put(presigned_url, files={'file': file})
+    file_path = 'C:/Users/91812/pttApp-apiTesting/tests/test_file/20230711-Test-api-Banking.xls'
+    df = pd.read_excel(file_path)
+
+    with open(file_path, 'rb') as file:
+        data = file.read()
+        response = requests.put(presigned_url, data=data, headers={"Content-Type": "application/vnd.ms-excel"})
         assert response.status_code == 200
 
     body = {
-        "clientId": "635132a4dc29b4bad9e74d90",
-        "fileName": "20230711-Test-api-Banking.xls",
+        "clientId": "63595f916a01b7ace483bcb1",
+        "fileName": "20230711-Test-api-Banking",
         "fileId": file_id
     }
 
     response = requests.post('https://dev-api.pttapp.com/api/banking/upload', headers=headers, json=body)
     print(response.content)
-    # assert response.status_code == 200
+    if response.status_code == 201:
+        print("Banking File Uploaded Successsfully")
+    elif response.status_code == 400:
+        error_message = response.content.decode('utf-8')
+        expected_error_message = '{"errors":["Previous scan for the item is not completed."]}\n'
+        assert error_message == expected_error_message, f"Expected: {expected_error_message}, Actual: {error_message}"
+        print("Error: Previous scan for the item is not completed.")
+    else:
+        print("Banking file upload failed")
+
+
+test_banking_file_upload()
